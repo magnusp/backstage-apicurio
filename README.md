d9cf4398b05beafaef407b4c5499374ed41fdd38
Magnus Persson <magnus.persson@fortnox.se>
2024-02-14 15:19:24 +0100

    Integrate with registry (apicurio)

--- README.md
+++ README.md
@@ -8,3 +8,36 @@ To start the app, run:
 yarn install
 yarn dev
 ```
+
+# Apicurio
+
+```
+docker run -it -p 8080:8080 apicurio/apicurio-registry-mem:latest-release
+
+curl --request POST \
+  --url http://localhost:8080/apis/registry/v2/groups/my-group/artifacts \
+  --header 'Content-type: application/json' \
+  --header 'X-Registry-ArtifactId: datalake-ref' \
+  --data '{
+  "openapi": "3.0.0",
+  "info": {
+    "version": "1.0.0",
+    "title": "Datalake API",
+    "license": {
+      "name": "MIT"
+    }
+  },
+  "paths": {
+    "/truncate": {
+      "delete": {
+        "summary": "Empty the lake",
+        "responses": {
+          "200": {
+            "description": "All gone!"
+          }
+        }
+      }
+    }
+  }
+}'
+```
--- packages/backend/src/plugins/catalog.ts
+++ packages/backend/src/plugins/catalog.ts
@@ -2,12 +2,17 @@ import { CatalogBuilder } from '@backstage/plugin-catalog-backend';
 import { ScaffolderEntitiesProcessor } from '@backstage/plugin-catalog-backend-module-scaffolder-entity-model';
 import { Router } from 'express';
 import { PluginEnvironment } from '../types';
+import { jsonSchemaRefPlaceholderResolver } from '@backstage/plugin-catalog-backend-module-openapi'
 
 export default async function createPlugin(
   env: PluginEnvironment,
 ): Promise<Router> {
   const builder = await CatalogBuilder.create(env);
   builder.addProcessor(new ScaffolderEntitiesProcessor());
+
+  builder.setPlaceholderResolver('openapi', jsonSchemaRefPlaceholderResolver);
+  builder.setPlaceholderResolver('asyncapi', jsonSchemaRefPlaceholderResolver);
+  
   const { processingEngine, router } = await builder.build();
   await processingEngine.start();
   return router;
